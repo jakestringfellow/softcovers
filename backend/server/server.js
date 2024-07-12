6d2813b3d73678c3a57b0ea2c965c567284aa570
wasadmin <wasadmin@example.com>
2024-07-12 14:41:45 -0400

    initial setup, configuring server and front end structure

---
+++ backend/server/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+console.log('Starting server...');
+
+app.get('/', (req, res) => {
+    res.send('Softcovers server');
+});
+
+app.listen(PORT, (err) => {
+    if (err) {
+        console.error('Failed to start server:', err);
+        return;
+    }
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+    <div>Softcovers Web App</div>
+  )
+}
+
+export default App
